d1c3ff295db81277196cbc303593422243144342
Knperera <Knperera.700@gmail.com>
2026-03-19 09:01:20 +0530

    login UI all done.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,6 +5,9 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+      final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
       backgroundColor: const Color(0XFF0a1625),
       body: SafeArea(
@@ -24,7 +27,7 @@ class LoginPage extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-                const SizedBox(height: 30),
+                const SizedBox(height: 10),
                 const Text(
                   'Welcome',
                   style: TextStyle(
@@ -33,14 +36,15 @@ class LoginPage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const SizedBox(height: 10),
+
+                const SizedBox(height: 0),
 
                 const Text(
                   'Please Sign to Track your expenses',
                   style: TextStyle(color: Colors.blueGrey, fontSize: 22),
                 ),
 
-                const SizedBox(height: 25),
+                const SizedBox(height: 20),
 
                 const Text(
                   'Email Address',
@@ -70,7 +74,7 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
 
-                const SizedBox(height: 25),
+                const SizedBox(height: 20),
 
                 const Text(
                   'Password',
@@ -82,6 +86,7 @@ class LoginPage extends StatelessWidget {
                 ),
 
                 const SizedBox(height: 10),
+
                 TextField(
                   style: const TextStyle(color: Colors.white),
                   decoration: InputDecoration(
@@ -100,7 +105,7 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
 
-                const SizedBox(height: 10),
+                const SizedBox(height: 5),
 
                 Align(
                   alignment: Alignment.centerRight,
@@ -113,31 +118,32 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
 
-                const SizedBox(height: 40),
+                const SizedBox(height: 10),
 
-                TextField(
-                  textAlign: TextAlign.center,
-                  enabled: false,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    // backgroundColor: Colors.blue,
+                Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue,
                   ),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.blue,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    hintText: 'Log In',
-                    hintStyle: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Text(
+                          "Log In",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
 
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
 
                 Align(
                   alignment: Alignment.center,
@@ -147,35 +153,123 @@ class LoginPage extends StatelessWidget {
                   ),
                 ),
 
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
 
-                TextField(
-                  textAlign: TextAlign.center,
-                  textAlignVertical: TextAlignVertical.center,
-                  enabled: false,
-                  style: const TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0XFF1c304a),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
+                Column(
+                  spacing: 20,
+                  children: [
+                    // const SizedBox(height: 25),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Color(0XFF1c304a),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon/google_icon.png',
+                            width: 25,
+                            height: 25,
+                            fit: BoxFit.contain,
+                          ),
+                          Center(
+                            child: Text(
+                              " Google",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    hintText: 'Google',
-                    hintStyle: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 23,
-                      fontWeight: FontWeight.bold,
+
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Color(0XFF1c304a),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon/apple_icon.png',
+                            width: 25,
+                            height: 25,
+                            fit: BoxFit.contain,
+                          ),
+                          Center(
+                            child: Text(
+                              " Apple",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'assets/icon/google_icon.png',
-                        width: 25,
-                        height: 25,
-                        fit: BoxFit.contain,
-                    )
-                  ),
-                ),
+
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Color(0XFF1c304a),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon/phone_icon.png',
+                            width: 25,
+                            height: 25,
+                            fit: BoxFit.contain,
+                          ),
+                          Center(
+                            child: Text(
+                              " Login with Mobile",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 0),
+
+                    Align(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 18,
+                          ),
+                          children: [
+                            const TextSpan(text: "Don't have an account? "),
+                            TextSpan(
+                              text: "Sign up",
+                              style: const TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
